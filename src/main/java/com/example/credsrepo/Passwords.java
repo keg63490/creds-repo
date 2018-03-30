63b000f12d8389f3f3562ece3e96683b6cd6e2ef
Grant Pinkley <gap47780@ucmo.edu>
2018-03-30 14:53:19 -0500

    Cleaned up and made some FindBug changes.

--- src/main/java/com/example/credsrepo/Passwords.java
+++ src/main/java/com/example/credsrepo/Passwords.java
@@ -9,10 +9,11 @@ import org.springframework.security.crypto.keygen.KeyGenerators;
 public class Passwords {
     final private static String secret = "Grant Pinkley";
 
-    private Passwords() {}
+    private Passwords() {
+    }
 
     //generate salt
-    public static String genNextSalt(){
+    public static String genNextSalt() {
         BytesKeyGenerator generator = KeyGenerators.secureRandom(16);
         final byte[] salt = generator.generateKey();
         final String salt1 = new String(Hex.encode(salt));
@@ -25,9 +26,6 @@ public class Passwords {
         //System.out.println("Original text: \"" + textToEncrypt + "\"");
         BytesEncryptor encryptor = Encryptors.standard(secret, passSalt);
         byte[] encryptedText = encryptor.encrypt(textToEncrypt);  //.getBytes());
-        String encryptedText1 = new String(Hex.encode(encryptedText));
-        //System.out.println("Encrypted byte: \"" + encryptedText + "\"");
-       // System.out.println("Encrypted text: \"" + encryptedText1 + "\"");
         return encryptedText;
     }
 
@@ -42,38 +40,4 @@ public class Passwords {
         //System.out.println("Decrypted text: \"" + decryptedText3 + "\"");
         return decryptedText3;
     }
-/*
-    public static void main(String [] args) throws UnsupportedEncodingException {
-        final String password = "Grant Pinkley";
-        BytesKeyGenerator generator = KeyGenerators.secureRandom(8);
-        final byte[] salt = generator.generateKey();
-                  //final String salt = KeyGenerators.string().generateKey();
-        final String salt1 = new String(Hex.encode(salt));
-
-        System.out.println(salt1);
-        String textToEncrypt = "This is a test!!";
-
-        //Encrypt
-        System.out.println("Original text: \"" + textToEncrypt + "\"");
-        BytesEncryptor encryptor = Encryptors.standard(password, salt1);
-        byte[] encryptedText = encryptor.encrypt(textToEncrypt.getBytes());
-        String encryptedText1 = new String(Hex.encode(encryptedText));
-        System.out.println("Encrypted text: \"" + encryptedText1 + "\"");
-
-        //Decrypt
-        BytesEncryptor decryptor = Encryptors.standard(password, salt1);
-        byte[] decryptedText = decryptor.decrypt(Hex.decode(encryptedText1));
-        String decryptedText2 = new String(Hex.encode(decryptedText));
-        String decryptedText3 = new String(Hex.decode(decryptedText2));
-        System.out.println("Decrypted text: \"" + decryptedText3 + "\"");
-
-       /* //Check if it worked
-        if(encryptedText1.equals(decryptedText2)) {
-            System.out.println("Success: decrypted text matches");
-        }
-        else {
-            System.out.println("Failed: decrypted text does not match");
-        }
-    }*/
 }
-
