15cb6694107e9fe42a5ef9f45a7ac644d527bd28
Grant Pinkley <gap47780@ucmo.edu>
2018-04-02 14:50:17 -0500

    Added try catches in password class and made changed .encode error from FindBugs.

--- src/main/java/com/example/credsrepo/Passwords.java
+++ src/main/java/com/example/credsrepo/Passwords.java
@@ -14,30 +14,42 @@ public class Passwords {
 
     //generate salt
     public static String genNextSalt() {
-        BytesKeyGenerator generator = KeyGenerators.secureRandom(16);
-        final byte[] salt = generator.generateKey();
-        final String salt1 = new String(Hex.encode(salt));
+            BytesKeyGenerator generator = KeyGenerators.secureRandom(16);
+            final byte[] salt = generator.generateKey();
+            final String salt1 = new String(Hex.encode(salt));
         return salt1;
     }
 
     //encrypt
     public static byte[] encrypt(byte[] textToEncrypt, String passSalt) {
         //Encrypt
-        //System.out.println("Original text: \"" + textToEncrypt + "\"");
-        BytesEncryptor encryptor = Encryptors.standard(secret, passSalt);
-        byte[] encryptedText = encryptor.encrypt(textToEncrypt);  //.getBytes());
+        byte[] encryptedText = null;
+        try {
+            BytesEncryptor encryptor = Encryptors.standard(secret, passSalt);
+            encryptedText = encryptor.encrypt(textToEncrypt);
+        }catch(Exception e) {
+            //do stuff
+        }
         return encryptedText;
+
     }
 
     //decrypt
     public static String decrypt(byte[] encryptedText, String passSalt) {
-        BytesEncryptor decryptor = Encryptors.standard(secret, passSalt);
-        //encryptedText = new String(Hex.decode(encryptedText));
-        //System.out.println(encryptedText);
-        byte[] decryptedText = decryptor.decrypt(encryptedText);  //.getBytes());
-        String decryptedText2 = new String(Hex.encode(decryptedText));
-        String decryptedText3 = new String(Hex.decode(decryptedText2));
-        //System.out.println("Decrypted text: \"" + decryptedText3 + "\"");
+        String decryptedText3=null;
+        try {
+            BytesEncryptor decryptor = Encryptors.standard(secret, passSalt);
+            //encryptedText = new String(Hex.decode(encryptedText));
+            //System.out.println(encryptedText);
+            byte[] decryptedText = decryptor.decrypt(encryptedText);  //.getBytes());
+            String decryptedText2 = new String(Hex.encode(decryptedText));
+            decryptedText3 = new String(Hex.decode(decryptedText2), "UTF-8");
+            //System.out.println("Decrypted text: \"" + decryptedText3 + "\"");
+
+        }catch (Exception e) {
+            //do stuff
+
+        }
         return decryptedText3;
     }
 }
